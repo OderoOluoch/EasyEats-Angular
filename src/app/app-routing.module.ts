aaa1e03768d6058cfd21b284eb968324a877823f
OderoOluoch <oderoluoch@gmail.com>
2021-08-19 01:27:47 +0300

    More styles updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ChoseUsComponent } from './components/chose-us/chose-us.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 const routes: Routes = [
-  
+  {path:'about', component:AboutComponent},
+  {path:'us', component:ChoseUsComponent},
+  {path:'menu', component:MenuComponent},
+  {path:'welcome', component:WelcomeComponent},
+  {path:'about', component:AboutComponent},
+  {path:'about', component:AboutComponent},
+  {path:'about', component:AboutComponent},
+
 ];
 
 @NgModule({
