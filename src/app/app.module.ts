563af021ea50e4442c78ff2b67feb38702a3045f
OderoOluoch <oderoluoch@gmail.com>
2021-08-19 15:44:31 +0300

    Restructuring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { UsComponent } from './components/us/us.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { OutletsComponent } from './components/outlets/outlets.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    UsComponent,
+    ContactComponent,
+    FooterComponent,
+    GalleryComponent,
+    HeaderComponent,
+    HomeComponent,
+    MenuComponent,
+    OutletsComponent,
+    WelcomeComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
